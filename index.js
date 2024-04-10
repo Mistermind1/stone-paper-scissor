d29feb7485dd95ee19991e310f068c82df9eb6b9
Exlaso <vedantbhavsar67@gmail.com>
2024-04-10 09:05:32 +0530

    rebuild index.js

--- index.js
+++ index.js
@@ -1,67 +1,34 @@
-const chooseUserChoice = (choice) => {
-  const userSelected = document.getElementById("userSelected");
-  
-  console.log(userSelected);
-  switch (choice) {
-    case "rock":
-      userSelected.src = "rock.png";
-      break;
-    case "paper":
-      userSelected.src = "paper.png";
-      break;
-    case "scissors":
-      userSelected.src = "scissors.png";
-      break;
-
-    default:
-      break;
-  }
-  StartComputerChoosing();
-};
-let cScore = 0;
-let uScore = 0;
-const Options = ["rock.png", "paper.png", "scissors.png"];
-const StartComputerChoosing = () => {
-  let duration = 100;
-  const interval = setInterval(() => {
-    console.log(Options.at(Math.floor(Math.random() * Options.length)));
-    document.getElementById("computerSelected").src = Options.at(
-      Math.floor(Math.random() * Options.length)
-    );
-  }, duration);
-  setTimeout(() => {
-    clearInterval(interval);
-    displayResult();
-  }, 3000);
-};
-
-const displayResult = () => {
-  const userSelected = document.getElementById("userSelected").src;
-  const computerSelected = document.getElementById("computerSelected").src;
-  const result = document.getElementById("result");
-  const computerScore = document.getElementById("Computerscore");
-  const userScore = document.getElementById("Userscore");
-
-  if (userSelected === computerSelected) {
-    result.innerHTML = "Tie!!";
+const Options = ["rock", "paper", "scissors"];
+const chooseUserChoice = (userSelectedOption) => {
+  console.log("user ", userSelectedOption);
+  const ComputerGeneratedOptions = Options[Math.floor(Math.random() * 3)];
+  console.log("computer ", ComputerGeneratedOptions);
+  if (userSelectedOption === ComputerGeneratedOptions) {
+    console.log("Tie");
   } else {
-    let userWin = true;
-    if (userSelected === "Rock") {
-      //paper scissors
-      userWin = computerSelected === "Paper" ? false : true;
-    } else if (userSelected === "Paper") {
-      //scissors , rock
-      userWin = computerSelected === "Scissors" ? false : true;
+    let UserWin = true;
+    if (userSelectedOption === "rock") {
+      UserWin =
+        ComputerGeneratedOptions === "paper"
+          ? false
+          : ComputerGeneratedOptions === "scissors"
+          ? true
+          : false;
+    } else if (userSelectedOption === "paper") {
+      UserWin =
+        ComputerGeneratedOptions === "scissors"
+          ? false
+          : ComputerGeneratedOptions === "rock"
+          ? true
+          : false;
     } else {
-      userWin = computerSelected === "Rock" ? false : true;
+      UserWin =
+        ComputerGeneratedOptions === "rock"
+          ? false
+          : ComputerGeneratedOptions === "paper"
+          ? true
+          : false;
     }
-    result.innerHTML = userWin ? "You Win" : "You Lose";
-    if (userWin) {
-      uScore++;
-      userScore.innerHTML = uScore;
-    } else {
-      cScore++;
-      computerScore.innerHTML = cScore;
-    }
+    console.log("user win", UserWin);
   }
 };
